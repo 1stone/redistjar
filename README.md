429f71f56eb3458f396521fb9e55602337ae651c
Joerg Delker <jd@onix.de>
2017-05-21 17:22:29 +0200

    added code for redistjar plugin

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
-# redistjar
-RedistJar Maven Plugin
+# RedistJar Maven Plugin
+
+This is a simple Maven plugin, that allows you to distribute pre-build JAR files
+to your preferred Maven repository in a more controlled fashion then you would
+do that with `mvn install:install-file ...`.
+
+It behaves pretty much like the well-known [Maven Jar Plugin](https://maven.apache.org/plugins/maven-jar-plugin/), which generates a jar artifact, but with the difference, that the default lifecycle is pretty much reduced to the package, install and deploy phases.
+
+
+## Getting Started
+
+To use this plugin, just clone the repository and install the plugin bits with
+
+    mvn install
+
+This should install you a copy of the plugin in your local maven repository.
+
+## Create RedistJar Project
+
+With the plugin installed, you can now create a new Maven Project for wrapping your pre-build jar you like to redistribute.
+For the following let's call that `MyPrebuild.jar`.
+
+Create a new maven project with a `pom.xml` that has this components:
+
+Make sure to use the jar build lifecycle:
+
+    <packaging>jar</packaging>
+
+Add any dependencies your JAR requires to run.
+In your example we require `commons-codec` and `commons-httpclient`. This will insure, that any project depending on your redistributed artifact is pulling the required libraries automatically.
+
+    <dependencies>
+      <dependency>
+        <groupId>commons-codec</groupId>
+        <artifactId>commons-codec</artifactId>
+        <version>1.3</version>
+        <scope>runtime</scope>
+      </dependency>
+      <dependency>
+        <groupId>commons-httpclient</groupId>
+        <artifactId>commons-httpclient</artifactId>
+        <version>3.1</version>
+        <scope>runtime</scope>
+      </dependency>
+    </dependencies>
+
+Finally, let maven know about the redistjar plugin that will replace the jar default lifecycle.
+Make sure to specify the location of your jar file in the configuration.
+
+    <build>
+      <plugins>     
+        <plugin>
+          <groupId>maven.plugin</groupId>
+          <artifactId>pseudojar-maven-plugin</artifactId>
+          <version>1.0-SNAPSHOT</version>
+          <extensions>true</extensions>
+          <configuration>
+            <jarFile>${basedir}/src/main/lib/myCustom.jar</jarFile>
+          </configuration>
+        </plugin>
+      </plugins>
+    </build>
+
+## Build your RedistJar Project
+
+With that `pom.xml` and the `MyCustom.jar` file in the specified location, you are ready to run.
+
+Install the jar with a proper pom.xml in your repository:
+
+    mvn install
+
+## Notes
+
+Effectively, all this has the same result as if you would have prepared a proper `pom.xml` (with dependencies!) and installed the jar manually with `mvn install:install-file ...` (following a ton of parameters).
+
+The problem with that is, you have to document all your steps somewhere, or put them in a batch file, to make that process reproducable later somehow.
+
+Bundling this into a nice and version-controlled maven project, makes this much more user-friendly.
+
+## Contributing
+
+Any contributions are welcome.
+
+## Authors
+
+* **Joerg Delker** - *Initial work* - [1stone](https://github.com/1stone)
+
+See also the list of [contributors](https://github.com/1stone/redistjar/graphs/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details
---
+++ src/main/java/onestone/maven/plugin/redistjar/JarMojo.java
@@ -0,0 +1,180 @@
+package onestone.maven.plugin.redistjar;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+
+/**
+ * Goal to redistribute a pre-build jar file
+ *
+ * @author "Joerg Delker <jd@onix.de>"
+ */
+@Mojo(name = "jar",
+        defaultPhase = LifecyclePhase.PACKAGE,
+        requiresProject = true,
+        threadSafe = true)
+public class JarMojo
+        extends AbstractMojo {
+
+  /**
+   * Pre-build JAR file that should be used as the main artifact.
+   */
+  @Parameter(required = true)
+  private File jarFile;
+
+  /**
+   * Directory containing the generated JAR.
+   */
+  @Parameter(defaultValue = "${project.build.directory}", required = true)
+  private File outputDirectory;
+
+  /**
+   * Name of the generated JAR.
+   */
+  @Parameter(defaultValue = "${project.build.finalName}", readonly = true)
+  private String finalName;
+
+  /**
+   * Classifier to add to the artifact generated. If given, the artifact will be
+   * attached as a supplemental artifact. If not given this will create the main
+   * artifact which is the default behavior. If you try to do that a second time
+   * without using a classifier the build will fail.
+   */
+  @Parameter
+  private String classifier;
+
+  /**
+   * The {@link {MavenProject}.
+   */
+  @Parameter(defaultValue = "${project}", readonly = true, required = true)
+  private MavenProject project;
+
+  /**
+   *
+   */
+  @Component
+  private MavenProjectHelper projectHelper;
+
+  /**
+   * {@inheritDoc}
+   */
+  protected String getClassifier() {
+    return classifier;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected String getType() {
+    return "jar";
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected File getJarFile() {
+    return jarFile;
+  }
+
+  /**
+   * @return the {@link #project}
+   */
+  protected final MavenProject getProject() {
+    return project;
+  }
+
+  /**
+   * Execute Plugin
+   * 
+   * @throws MojoExecutionException
+   * @throws MojoFailureException 
+   */
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    File targetFile = provideArchive();
+
+    if (hasClassifier()) {
+      projectHelper.attachArtifact(getProject(), getType(), getClassifier(), targetFile);
+    } else {
+      if (projectHasAlreadySetAnArtifact()) {
+        throw new MojoExecutionException("You have to use a classifier "
+                + "to attach supplemental artifacts to the project instead of replacing them.");
+      }
+      getProject().getArtifact().setFile(targetFile);
+    }
+  }
+
+  /**
+   * Returns the Jar file to generate, based on an optional classifier.
+   *
+   * @param basedir the output directory
+   * @param resultFinalName the name of the ear file
+   * @param classifier an optional classifier
+   * @return the file to generate
+   */
+  protected File getTargetJarFile(File basedir, String resultFinalName, String classifier) {
+    if (basedir == null) {
+      throw new IllegalArgumentException("basedir is not allowed to be null");
+    }
+    if (resultFinalName == null) {
+      throw new IllegalArgumentException("finalName is not allowed to be null");
+    }
+
+    StringBuilder fileName = new StringBuilder(resultFinalName);
+
+    if (hasClassifier()) {
+      fileName.append("-").append(classifier);
+    }
+
+    fileName.append(".jar");
+
+    return new File(basedir, fileName.toString());
+  }
+
+  protected File provideArchive() throws MojoExecutionException {
+    File targetFile = getTargetJarFile(outputDirectory, finalName, getClassifier());
+    try {
+      File dir = targetFile.getParentFile();
+      if (!dir.isDirectory()) {
+        Files.createDirectory(targetFile.getParentFile().toPath());
+      }
+      Files.copy(jarFile.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    } catch (IOException ex) {
+      getLog().error("Error during copying from " + jarFile + " to " + targetFile);
+      throw new MojoExecutionException("Error copying JAR from ", ex);
+    }
+    return targetFile;
+  }
+
+  private boolean projectHasAlreadySetAnArtifact() {
+    if (getProject().getArtifact().getFile() != null) {
+      return getProject().getArtifact().getFile().isFile();
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * @return true in case where the classifier is not {@code null} and contains
+   * something else than white spaces.
+   */
+  protected boolean hasClassifier() {
+    boolean result = false;
+    if (getClassifier() != null && getClassifier().trim().length() > 0) {
+      result = true;
+    }
+
+    return result;
+  }
+}
